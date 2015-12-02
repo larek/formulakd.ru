b7b354d6f4abaa27a325ca5e691757eba9128880
Alexandr Tikhonov <t9101029991@gmail.com>
2015-12-02 19:44:55 +0300

    db_config

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=formulakd',
-    'username' => 'formulakd',
-    'password' => 'avatar910',
+    'username' => 'root',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
