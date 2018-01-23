f8cd6347ae67e6a323d1d488b4a26908027296de
Aleksandr Tikhonov <t9101029991@gmail.com>
2018-01-23 15:29:59 +0300

    new service page

--- basic/views/site/services.php
+++ basic/views/site/services.php
@@ -8,23 +8,91 @@ $this->title = 'Услуги';
     </div>
   </div>
 </div>
-<div class="container mt-3">
-  <div class="row">
-    <div class="col-md-6">
-      <img src="/images/services.jpg" class='img-fluid' alt="">
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3 col-6 text-center mt-4">
+            <img src="/demo/images/service-image-1.jpg" class='img-fluid rounded-circle' alt="">
+            <a href="#" class='potencial_item_link'>Проектирование <br>и расчеты</a>
+            <div class="service-block-description">
+              Проектирование – это поиск научно-обоснованных, технически осуществимых и экономически эффективных инженерных решений для изготовления изделия. На этом этапе анализируется большой поток вводных данных, исходя из которых разрабатываются варианты конструкций, возможные технологии изготовления и допустимость использования изделия. При этом проводятся необходимые расчеты и сравниваются все возможные комбинации.
+            </div>
+        </div>
+        <div class="col-md-3 col-6  text-center mt-4">
+            <img src="/demo/images/service-image-2.jpg" class='img-fluid rounded-circle' alt="">
+            <a href="#" class='potencial_item_link'>Разработка <br>документации</a>
+            <div class="service-block-description">
+            Мы ответственно подходим к вопросу изготовления конструкторской документации и выполняем ее по общепринятым у специалистов нормам в соответствии с существующими российскими стандартами на изготовление подобных документов, тем самым предоставив полную информацию об изделии. На данный момент, в нашей стране принят единый национальный стандарт изготовления конструкторской документации – ЕСКД.
+          </div>
+        </div>
+        <div class="col-md-3 col-6  text-center mt-4">
+            <img src="/demo/images/service-image-3.jpg" class='img-fluid rounded-circle' alt="">
+            <a href="#" class='potencial_item_link'>Изготовление <br>оборудования</a>
+            <div class="service-block-description">
+              Уверенно предлагать полный комплекс работ нам позволяют налаженные связи с поставщиками и наличие собственной производственной площади, которая оснащена всем необходимым оборудованием для резки и обработки металла. В дополнение к этому большой опыт и высокая квалификация производственного персонала нашего конструкторского бюро гарантируют высокое качество работ по сборке и монтажу оборудования.
+            </div>
+        </div>
+        <div class="col-md-3 col-6 text-center mt-4">
+            <img src="/demo/images/service-image-4.jpg" class='img-fluid rounded-circle' alt="">
+            <a href="#" class='potencial_item_link'>Разработка <br>систем управления</a>
+            <div class="service-block-description">
+              Исходя из конкретных требований заказчиков мы самостоятельно разрабатываем программное обеспечение для автоматизации процесса сбора данных и управления оборудованием. К функциональной точности и быстродействию программы мы всегда стараемся разработать интуитивно понятный и эстетически приятный интерфейс для удобства использования оборудования.
+            </div>
+        </div>
     </div>
-    <div class="col-md-6">
-      <h6>ПРОЕКТИРОВАНИЕ И РАСЧЕТЫ</h6>
-      <p>Проектирование – это поиск научно-обоснованных, технически осуществимых и экономически эффективных инженерных решений для изготовления изделия. На этом этапе анализируется большой поток вводных данных, исходя из которых разрабатываются варианты конструкций, возможные технологии изготовления и допустимость использования изделия. При этом проводятся необходимые расчеты и сравниваются все возможные комбинации.</p>
-
-      <h6>РАЗРАБОТКА ДОКУМЕНТАЦИИ</h6>
-      <p>Мы ответственно подходим к вопросу изготовления конструкторской документации и выполняем ее по общепринятым у специалистов нормам в соответствии с существующими российскими стандартами на изготовление подобных документов, тем самым предоставив полную информацию об изделии. На данный момент, в нашей стране принят единый национальный стандарт изготовления конструкторской документации – ЕСКД.</p>
-
-      <h6>ИЗГОТОВЛЕНИЕ ОБОРУДОВАНИЯ</h6>
-      <p>Уверенно предлагать полный комплекс работ нам позволяют налаженные связи с поставщиками и наличие собственной производственной площади, которая оснащена всем необходимым оборудованием для резки и обработки металла. В дополнение к этому большой опыт и высокая квалификация производственного персонала нашего конструкторского бюро гарантируют высокое качество работ по сборке и монтажу оборудования. </p>
-
-      <h6>РАЗРАБОТКА СИСТЕМ УПРАВЛЕНИЯ</h6>
-      <p>Исходя из конкретных требований заказчиков мы самостоятельно разрабатываем программное обеспечение для автоматизации процесса сбора данных и управления оборудованием. К функциональной точности и быстродействию программы мы всегда стараемся разработать интуитивно понятный и эстетически приятный интерфейс для удобства использования оборудования.</p>
+</div>
+<div class="container mt-4">
+  <div class="col-md-12 col-xs-12 callback-wrapper">
+    <div class="callback-title">НЕ ТРАТЬТЕ ВРЕМЯ НА ЛИШНИЕ ПОИСКИ И РАЗМЫШЛЕНИЯ - </div>
+    <div class="callback-subtitle">проконсультируйтесь со специалистом уже сейчас</div>
+    <div class="form-wrap">
+        <div class="row">
+        <div class="col-md-4 center mt-1"><input type="text" class="form-control callback-widget-name" placeholder="Имя*"></div>
+        <div class="col-md-4 center mt-1"><input type="text" class="form-control callback-widget-phone" placeholder="Телефон*"></div>
+        <div class="col-md-4 center mt-1"><span class="btn btn-primary btn-block btn-widget-callback">Получить консультацию</span></div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="alert alert-info alert-widget-callback" style="display:none;">Заявка отправлена. Благодарим Вас за интерес, проявленный к нашей компании. <br>Мы обязательно ответим Вам в самое ближайшее время</div>
+        </div>
     </div>
   </div>
+</div>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="row">
+                <div class="col-6 col-md-12 text-center mt-3">
+                    <img src="/images/mashinka.svg" class='img-fluid small' alt="">
+                    <div class="workmap_row">Организация</div>
+                    <div class="workmap_row">транспортных услуг</div>
+                    <div class="workmap_row">по доставке</div>
+                    <div class="workmap_row">оборудования</div>
+                </div>
+                <div class= "col-6  col-md-12 text-center mt-3">
+                    <img src="/images/shesterenka.svg" class='img-fluid small' alt="">
+                    <div class="workmap_row">Послепродажное</div>
+                    <div class="workmap_row">обслуживание</div>
+                    <div class="workmap_row">и гарантия качества</div>
+                    <div class="workmap_row">оборудования</div>
+                </div>
+                <div class="col-6  col-md-12 text-center mt-3">
+                    <img src="/images/dokument.svg" class='img-fluid small' alt="">
+                    <div class="workmap_row"><Организация</div>
+                    <div class="workmap_row">и проведение</div>
+                    <div class="workmap_row">первичной аттестации</div>
+                    <div class="workmap_row">оборудования</div>
+                </div>
+                <div class="col-6 col-md-12 text-center mt-3">
+                    <img src="/images/instrument.svg" class='img-fluid small' alt="">
+                    <div class="workmap_row">Проведение монтажных</div>
+                    <div class="workmap_row">и пуско-наладочных</div>
+                    <div class="workmap_row">работ,</div>
+                    <div class="workmap_row">инструктаж персонала</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 mt-3 workmap-shestermi-wrapper">
+            <img src="/images/karta_shesterni_e.jpg" class='img-fluid' alt="">
+        </div>
+    </div>
 </div>
