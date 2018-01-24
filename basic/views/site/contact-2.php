031e9e8daa8e29b61c5c7aa4a007901a9f73d607
Aleksandr Tikhonov <t9101029991@gmail.com>
2018-01-24 11:42:04 +0300

    new footer

--- basic/views/site/contact-2.php
+++ basic/views/site/contact-2.php
@@ -1,3 +1,58 @@
 <?
 $this->title = 'Контакты';
 ?>
+ <div class="map-wrapper">
+
+        <div class="d-block d-md-none text-center mt-4">
+            <a class='btn btn-secondary' data-fancybox="iframe" data-src="https://yandex.ru/map-widget/v1/?um=constructor%3AoFyOK4TT-dveXO0gw1jugftdQYfwy6Z6&amp;source=constructor" data-type="iframe" data-width='1000px' href="javascript:;">Посмотреть на карте</a>
+        </div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7"></div>
+                <div class="col-md-4 p-0">
+                    <div class="contact-block">
+                        <div class="row">
+                            <div class="col-12 text-center d-none d-md-block">
+                                <span class="contact-title">Наши контакты</span>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-2"><img src="/demo/images/contact-icon-1.svg" class='img-fluid' alt=""></div>
+                            <div class="col-10">
+                                <b>Адрес КБ</b>
+                                <br> г. Нижний Новгород
+                                <br> пер. Мотальный, д. 10, пом. 8-9
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-2"><img src="/demo/images/contact-icon-2.svg" class='img-fluid' alt=""></div>
+                            <div class="col-10">
+                                <b>Адрес производства</b>
+                                <br> г. Нижний Новгород,
+                                <br> Московское шоссе, д.105
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-2"><img src="/demo/images/contact-icon-3.svg" class='img-fluid' alt=""></div>
+                            <div class="col-10">
+                                <b><a href="mailto:info@formulakd.ru" class='blue-link'>info@formulakd.ru</a></b>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-2"><img src="/demo/images/contact-icon-4.svg" class='img-fluid' alt=""></div>
+                            <div class="col-10">
+                                <b><a href="tel:+78312815878" class='blue-link'>8 (831) 281-58-78</a></b>
+                                <br>
+                                <b><a href="tel:+78312815898" class='blue-link'>8 (831) 281-58-98</a></b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-none d-md-block mt-4">
+         <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3AoFyOK4TT-dveXO0gw1jugftdQYfwy6Z6&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=true"></script>
+        </div>
+    </div>
